0650b7f2e35580369d0c0e245dd7ae56836e46bb
Viqo07 <vicomus19@gmail.com>
2026-03-12 22:03:28 -0500

    Add files via upload

---
+++ Booking.cs
@@ -0,0 +1,12 @@
+﻿public class Booking
+{
+	public string RoomNumber { get; set; }
+	public string GuestName { get; set; }
+	public DateTime CheckIn { get; set; }
+	public DateTime CheckOut { get; set; }
+
+	public override string ToString()
+	{
+		return $"[{RoomNumber}] {CheckIn:g} - {CheckOut:g} {GuestName}";
+	}
+}
