02c506d43c0271dd73876f3bd10e474e1ac90c79
Viqo07 <vicomus19@gmail.com>
2026-03-12 22:03:42 -0500

    Add files via upload

---
+++ BookingManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BookingManager
+{
+	private List<Booking> bookings = new List<Booking>();
+
+	public bool AddBooking(Booking b)
+	{
+		if (!CheckAvailability(b.RoomNumber, b.CheckIn, b.CheckOut))
+			return false;
+		bookings.Add(b);
+		return true;
+	}
+
+	public bool CancelBooking(string roomNumber, string guestName)
+	{
+		Booking toRemove = bookings.Find(b => b.RoomNumber == roomNumber && b.GuestName == guestName);
+		if (toRemove != null)
+		{
+			bookings.Remove(toRemove);
+			return true;
+		}
+		return false;
+	}
+
+	public List<Booking> ListAll()
+	{
+		return bookings;
+	}
+
+	public bool CheckAvailability(string roomNumber, DateTime checkIn, DateTime checkOut)
+	{
+		foreach (var b in bookings)
+		{
+			if (b.RoomNumber == roomNumber &&
+			   !(checkOut <= b.CheckIn || checkIn >= b.CheckOut))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
